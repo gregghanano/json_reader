d1062369417be78979b4056bc847c9d82aa166a1
Gregg Hanano <gregg@Unitys-MacBook-Air-7.local>
2015-12-15 18:18:05 -0800

    updated json reader to display text

--- server.js
+++ server.js
@@ -11,33 +11,57 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname+'/static'));
 
-var obj = {
-  "alfonso":["1","2","3"],
-  "kristal":["4","5","6"],
-  "gregg":["7","8","9"]
-};
+var obj = {};
 app.get('/', function(req, res){
   //obj = JSON.parse(fs.readFileSync('output.json', 'utf8'));
-  var names = ['alfonso','kristal','gregg'];
-  res.render('index',{users:names});
-})
-
-app.get('/users/:name', function(req, res){
-  var key = req.path.split('/')[2];
-  console.log(key);
-  var obj = {};
+  var namesLookUp ={};
+  var amNames = [];
   fs.readFile('./output.json','utf8', function(err,data){
     console.log("reading file");
     if(err) throw err;
     obj = JSON.parse(data);
+    // console.log(obj);
+    for(var i in obj){
+      //console.log(obj[i].amContact);
+      if(!(namesLookUp.hasOwnProperty(obj[i].amContact))){
+        namesLookUp[obj[i].amContact] = true;
+        amNames.push(obj[i].amContact);
+      }
+    }
+    //console.log(amNames);
+    var object = {users: amNames};
+    //console.log(object);
+    res.render('index',object);
   })
+})
+
+app.get('/users/:name', function(req, res){
   console.log(obj);
-  //res.render('user', {user : obj[key], name: key});
+  var amName = req.path.split('/')[2];
+  console.log(amName);
+  var userObj = {};
+  userObj.name = amName;
+  userObj.userReasons = [];
+  for(var i in obj){
+    if(obj[i].amContact === amName){
+      console.log('yes! ' + amName);
+      console.log('reasons ' + obj[i].reasons.join(''));
+      userObj.userReasons.push(obj[i].reasons.join(''));
+    }
+  }
+  for(var j = 0; j < userObj.userReasons.length; j++){
+    userObj.userReasons[j] = userObj.userReasons[j].replace(/<br\/\>/g, "/n");
+    userObj.userReasons[j] = userObj.userReasons[j].replace(/&nbsp;/g, " ");
+    userObj.userReasons[j] = userObj.userReasons[j].replace(/<p>/g, "");
+    userObj.userReasons[j] = userObj.userReasons[j].replace(/<\/p>/g,"");
+  }
+  console.log("/////////////",userObj);
+  res.render('user', {user: userObj});
 })
 
 app.listen(8000, function(){
   console.log('listening on port 8000');
 })
 
-//db.installs.find({targetGame:67271,created:{$gt:ISODate("2015-11-30"), $lt:ISODate("2015-12-13")}},{_id:0,created:1,"identity.rawAdvertisingTrackingId":1,sourceGame:1, targetGame:1})
-//db.installs.find({campaignDeveloperId: 1423, targetGame:{$in: [82616,81633]}, created:{$gt:ISODate("2015-09-30")}},{_id:0, created:1, sourceGame:1,targetGame:1,"identity.rawAdvertisingTrackingId":1})
+// db.installs.find({targetGame:67271,created:{$gt:ISODate("2015-11-30"), $lt:ISODate("2015-12-13")}},{_id:0,created:1,"identity.rawAdvertisingTrackingId":1,sourceGame:1, targetGame:1})
+// db.installs.find({campaignDeveloperId: 1423, targetGame:{$in: [82616,81633]}, created:{$gt:ISODate("2015-09-30")}},{_id:0, created:1, sourceGame:1,targetGame:1,"identity.rawAdvertisingTrackingId":1})
